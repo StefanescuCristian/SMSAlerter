2fa07a23f21ba2e6532009f984e5110d587ad8b5
Stefanescu Cristian <howerkraft@gmail.com>
2012-06-19 17:29:55 +0300

    fixed a little mistake in CPU script

--- CPU_HDD_temp.sh
+++ CPU_HDD_temp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 hdd=$( hddtemp /dev/sda | awk '{print $NF}' | tr -d "[°C]" )
-cpu=$( sensors | tail -2 | head -1 | awk '{print $3}' | sed 's/^.//' | tr -d "[°C]" )
+cpu=$( sensors | grep 'Core 0:' | awk '{print $3}' | sed 's/^.//' | tr -d "[°C]" )
 cpu_temp_alert=60.0
 
 if [ $(echo "$cpu > $cpu_temp_alert" | bc) -eq 1 ]; then
