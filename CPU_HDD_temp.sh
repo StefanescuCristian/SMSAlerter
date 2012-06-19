b22e30d66ec98299102282076b092958a93c9f24
Stefanescu Cristian <howerkraft@gmail.com>
2012-06-19 16:31:40 +0300

    Added CPU&HDD temperature script

--- CPU_HDD_temp.sh
+++ CPU_HDD_temp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 hdd=$( hddtemp /dev/sda | awk '{print $NF}' | tr -d "[°C]" )
 cpu=$( sensors | tail -2 | head -1 | awk '{print $3}' | sed 's/^.//' | tr -d "[°C]" )
-cpu_temp_alerta=60.0
+cpu_temp_alert=60.0
 
-if [ $(echo "$cpu > $cpu_temp_alerta" | bc) -eq 1 ]; then
-ttytter -silent -status="Temperatura CPU: $cpu°C. Temperatura HDD: $hdd°C"
+if [ $(echo "$cpu > $cpu_temp_alert" | bc) -eq 1 ]; then
+ttytter -silent -status="CPU temperature: $cpu°C. HDD temperature: $hdd°C"
 fi
