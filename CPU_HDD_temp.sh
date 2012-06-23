cd6d0fe38f034344c928cb48dd5b221c23b32231
Stefanescu Cristian <howerkraft@gmail.com>
2012-06-23 19:04:28 +0300

    Fixed HDD temperature not being shown

--- CPU_HDD_temp.sh
+++ CPU_HDD_temp.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-hdd=$( hddtemp /dev/sda | awk '{print $NF}' | tr -d "[°C]" )
+hddtemp /dev/sda > /tmp/hdd
+hadd=$( cat /tmp/hdd | awk '{print $NF}' | tr -d "[°C]" )
 cpu=$( sensors | grep 'Core 0:' | awk '{print $3}' | sed 's/^.//' | tr -d "[°C]" )
 cpu_temp_alert=60.0
 
 if [ $(echo "$cpu > $cpu_temp_alert" | bc) -eq 1 ]; then
-ttytter -silent -status="CPU temperature: $cpu°C. HDD temperature: $hdd°C"
+ttytter -silent -status="CPU temperature: $cpu°C. HDD temperature: $hadd°C"
 fi
